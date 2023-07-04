d56b648d34e6893e38b8980959f147591a4c9450
SirBeho <benjamin.tavarez.98@gmail.com>
2023-07-04 09:25:59 -0400

    estructura main

--- app/page.jsx
+++ app/page.jsx
@@ -10,7 +10,31 @@ export default function Home() {
         <div className=" d-flex row min-vh-100 vw-100 text-white">
            <SideBar/>
            <div className="col-sm-8 d-flex justify-content-center" style={{background:"#100e1d"}}>
-             
+            <div className="p-3" style={{ maxWidth:"700px"}}>
+                <div className="d-flex mb-3 py-2 gap-2 justify-content-end">
+                    botones
+                    botones
+                </div>
+                <div className="d-flex gap-4 my-5 justify-content-center flex-wrap" >
+                    cartas
+                    cartas
+                    cartas
+                    cartas
+                </div>
+                <div className="p-2">
+                    <span className="fs-4 fw-semibold">Today&apos;s Hightlights</span>
+                    <div className="row gap-3 p-3 justify-content-center justify-content-md-between ">
+                        bigcartas
+                        bigcartas
+                        bigcartas
+                        bigcartas
+                    </div>
+                </div>
+                <footer>
+                    <span>Create by Benjamin</span>
+                </footer>
+            </div>    
+           
             </div>
         </div>
     )
